841caae26ab91e2d5d5aa7311313d5b8fa7b6a62
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2021-03-24 12:08:47 -0230

    Fixed - printer attempting to print past X=10 when using entire 250mm of print bed.
    When printing parts of size > 240mm, this version of the firmware will attempt to compress the entire left 10mm of the part into one line. Causing all sorts of odd behavior. Setting this to 0 solves this issue.

--- Marlin_ER20/Marlin/Configuration.h
+++ Marlin_ER20/Marlin/Configuration.h
@@ -1093,7 +1093,7 @@
 #define Y_BED_SIZE 226 
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS  10
+#define X_MIN_POS  0
 #define Y_MIN_POS  -8
 #define Z_MIN_POS 0
 #define X_MAX_POS (X_BED_SIZE + X_MIN_POS)
