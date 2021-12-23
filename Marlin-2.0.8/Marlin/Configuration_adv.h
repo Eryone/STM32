3c9157a1627ce2225d9056f4272e50cf5a86eaa5
Eryone <eryone3d@gmail.com>
2021-12-23 18:04:39 +0800

    Update Configuration_adv.h
    Adjusted the tmc current and motherboard cooling speed

--- Marlin-2.0.8/Marlin/Configuration_adv.h
+++ Marlin-2.0.8/Marlin/Configuration_adv.h
@@ -555,10 +555,10 @@
 
 #define EXTRUDER_AUTO_FAN_TEMPERATURE 50
 #define EXTRUDER_AUTO_FAN_SPEED 255   // 255 == full speed
-#define CHAMBER_AUTO_FAN_TEMPERATURE 40
-#define CHAMBER_AUTO_FAN_SPEED 130
-#define COOLER_AUTO_FAN_TEMPERATURE 40
-#define COOLER_AUTO_FAN_SPEED 130
+#define CHAMBER_AUTO_FAN_TEMPERATURE 20 //40
+#define CHAMBER_AUTO_FAN_SPEED 200 //130
+#define COOLER_AUTO_FAN_TEMPERATURE 20 //40
+#define COOLER_AUTO_FAN_SPEED 200 //130
 
 /**
  * Part-Cooling Fan Multiplexer
@@ -2538,7 +2538,7 @@
   #define INTERPOLATE      true
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       800        // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT       700 //800        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16        // 0..256
     #define X_RSENSE          0.11
@@ -2556,7 +2556,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       800
+    #define Y_CURRENT       700 //800
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
@@ -2610,7 +2610,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      800
+    #define E0_CURRENT      700 //800
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
